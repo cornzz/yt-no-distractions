f28060bee624f4fb03cb64cd78fa7016e2ce531b
cornzz <39997278+cornzz@users.noreply.github.com>
2023-10-15 04:27:19 +0200

    Initial commit

---
+++ yt-no-distractions.js
@@ -0,0 +1,14 @@
+console.log('loaded yt-no-distractions.')
+
+let targets = ['#masthead-container', '.html5-endscreen', '#columns']
+
+const observer = new MutationObserver(() => {
+    document.querySelectorAll(targets.join(', ')).forEach((element) => {
+        const attributes = `${element.id} ${element.classList.value}`
+        element.remove()
+        targets = targets.filter(target => !attributes.includes(target.slice(1)))
+    })
+    if (!targets.length) observer.disconnect()
+})
+
+observer.observe(document.getElementById('content'), { subtree: true, childList: true })
