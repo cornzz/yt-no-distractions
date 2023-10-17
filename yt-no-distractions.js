af77c85020ff97e1abc455eaa26d82e76e4fbc18
cornzz <39997278+cornzz@users.noreply.github.com>
2023-10-17 05:31:56 +0200

    Extend target selectors, fix for embedded videos, block landing page

--- yt-no-distractions.js
+++ yt-no-distractions.js
@@ -1,14 +1,25 @@
 console.log('loaded yt-no-distractions.')
 
-let targets = ['#masthead-container', '.html5-endscreen', '#below', '#secondary']
+const watch = window.location.pathname.startsWith('/watch')
+const embed = window.location.pathname.startsWith('/embed')
+const allowLanding = localStorage.getItem('allowLanding')
 
-const observer = new MutationObserver(() => {
-    document.querySelectorAll(targets.join(', ')).forEach((element) => {
-        const attributes = `${element.id} ${element.classList.value}`
-        element.remove()
-        targets = targets.filter(target => !attributes.includes(target.slice(1)))
+if (watch || embed) {
+    let targets = [
+        '.html5-endscreen',
+        ...(!embed ? ['#masthead-container', '#below', '#secondary'] : ['.ytp-pause-overlay-container'])
+    ]
+    
+    const observer = new MutationObserver(() => {
+        document.querySelectorAll(targets.join(', ')).forEach((element) => {
+            element.remove()
+            const attributes = `${element.id} ${element.classList.value}`
+            targets = targets.filter(target => !attributes.includes(target.slice(1)))
+        })
+        if (!targets.length) observer.disconnect()
     })
-    if (!targets.length) observer.disconnect()
-})
-
-observer.observe(document.body, { subtree: true, childList: true })
+    
+    observer.observe(document.body, { subtree: true, childList: true })
+} else if (!allowLanding) {
+    document.body.remove()
+}
